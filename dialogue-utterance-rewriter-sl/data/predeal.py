e6323a19e14a5b96fb6eea85dc6d3b83624663d0
nonstopfor <zx-zhang18@mails.tsinghua.edu.cn>
2020-04-02 17:53:45 +0800

    dataset split

--- dialogue-utterance-rewriter-sl/data/predeal.py
+++ dialogue-utterance-rewriter-sl/data/predeal.py
@@ -1,20 +1,26 @@
 import json
 
-in_file=open('corpus.txt','r',encoding='utf-8')
+in_file = open('corpus.txt', 'r', encoding='utf-8')
 
-out_file=open('train.json','w',encoding='utf-8')
+out_file_train = open('train.json', 'w', encoding='utf-8')
+out_file_valid = open('valid.json', 'w', encoding='utf-8')
+out_file_test = open('test.json', 'w', encoding='utf-8')
+lines = in_file.readlines()
 
-lines=in_file.readlines()
-
-cnt=0
+cnt = 0
 for line in lines:
-    line=line.split()
-    post=' '.join(line[:-1])
-    resp=line[-1]
-    d={'post':post,'resp':resp}
-    json.dump(d,out_file)
-    out_file.write('\n')
-    if cnt==0:
-        print(line)
-        print(post)
-    cnt+=1
+    line = line.split()
+    post = ' '.join(line[:-1])
+    resp = line[-1]
+    d = {'post': post, 'resp': resp}
+    if cnt < 18000:
+        json.dump(d, out_file_train)
+        out_file_train.write('\n')
+    elif cnt < 19000:
+        json.dump(d, out_file_valid)
+        out_file_valid.write('\n')
+    else:
+        json.dump(d, out_file_test)
+        out_file_test.write('\n')
+
+    cnt += 1
--- dialogue-utterance-rewriter-sl/main.py
+++ dialogue-utterance-rewriter-sl/main.py
@@ -1,6 +1,6 @@
 import torch
 
-from torchtext.data import Field
+from torchtext.data import Field, TabularDataset
 import numpy as np
 
 import random
@@ -19,13 +19,27 @@ def tokenize_post(s):
     sentences = s.split(' ')
     r = []
     for sentence in sentences:
-        r.append(jieba.lcut(sentence))
+        r.extend(jieba.lcut(sentence))
+        r.append('<eos>')
     r.pop(-1)
     return r
 
-def tokenize_response(s):
+
+def tokenize_resp(s):
     return jieba.lcut(s)
+
 
 def main():
     setup_seed(2020)
-    SRC = Field()
+    POST = Field(tokenize=tokenize_post, init_token='<sos>', eos_token='<eos>')
+    RESP = Field(tokenize=tokenize_resp, init_token='<sos>', eos_token='<eos>')
+    fields = {'post': ('post', POST), 'resp': ('resp', RESP)}
+    train_data, valid_data, test_data = TabularDataset.splits(path='./data', train='train.json'
+                                                              , validation='valid.json', test='test.json',
+                                                              format='json', fields=fields)
+    POST.build_vocab(train_data, min_freq=1)
+    RESP.build_vocab(train_data, min_freq=1)
+
+
+if __name__ == '__main__':
+    main()
