1e4a62c6451692564631f2861af6ec93d199eec4
陈炜 <chenwei@chenweideMacBook-Air.local>
2020-04-03 17:24:52 +0800

    #1 add iter

--- dialogue-utterance-rewriter-sl/main.py
+++ dialogue-utterance-rewriter-sl/main.py
@@ -1,6 +1,7 @@
 import torch
 
 from torchtext.data import Field, TabularDataset
+from torchtext.data import BucketIterator
 import numpy as np
 
 import random
@@ -29,6 +30,7 @@ def tokenize_resp(s):
     return jieba.lcut(s)
 
 
+
 def main():
     setup_seed(2020)
     POST = Field(tokenize=tokenize_post, init_token='<sos>', eos_token='<eos>')
@@ -39,6 +41,14 @@ def main():
                                                               format='json', fields=fields)
     POST.build_vocab(train_data, min_freq=1)
     RESP.build_vocab(train_data, min_freq=1)
+
+    train_iter, val_iter, test_iter = BucketIterator.splits((train_data, valid_data, test_data), 
+                                                            batch_sizes=(10,10,10), 
+                                                            sort_key=lambda x: len(x.text),
+                                                            sort_with_batch=True, # sort according to the len, for padding in LSTM
+                                                            repeat=False) 
+    
+    
 
 
 if __name__ == '__main__':
